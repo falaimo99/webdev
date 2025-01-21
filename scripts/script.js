65eba2e1384b85c15d6077ddab54c16553e7137d
Francesco Alaimo <f.alaimo99@gmail.com>
2025-01-21 10:54:10 +0100

    feat(styling to map): applied new styles, buttons and utilities to the map element, TODO: Welcome dialog function with the possibility of selecting there path and difficulty of the visit and real map dialog for accessibility and orienting reasons

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,18 @@
-const map = (() => {
-	let a = document.querySelectorAll("a");
-	let dialog = document.querySelector("dialog");
-	let span = document.querySelector("#closeDialog");
-	span.addEventListener("click", () => {
-		dialog.close();
-	});
-	a.forEach((a) =>
-		a.addEventListener("click", () => {
-			dialog.showModal();
-		})
-	);
+// const map = (() => {
+// 	let a = document.querySelectorAll("a");
+// 	let dialog = document.querySelector("dialog");
+// 	let span = document.querySelector("#closeDialog");
+// 	span.addEventListener("click", () => {
+// 		dialog.close();
+// 	});
+// 	a.forEach((a) =>
+// 		a.addEventListener("click", () => {
+// 			dialog.showModal();
+// 		})
+// 	);
+// })();
+
+const welcome = (() => {
+	let welcomeDialog = document.createElement("dialog");
+	let container = document.querySelector("#grid-container");
 })();
