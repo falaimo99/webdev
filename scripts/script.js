ba75c65e6b3da7d0e6bb3ae14c58096575d6dfef
Francesco Alaimo <f.alaimo99@gmail.com>
2025-01-29 12:11:44 +0100

    feat(color and script): added some themed coloring to buttons and correct layout when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,31 @@
-// const map = (() => {
-// 	let a = document.querySelectorAll("a");
-// 	let dialog = document.querySelector("dialog");
-// 	let span = document.querySelector("#closeDialog");
-// 	span.addEventListener("click", () => {
-// 		dialog.close();
-// 	});
-// 	a.forEach((a) =>
-// 		a.addEventListener("click", () => {
-// 			dialog.showModal();
-// 		})
-// 	);
-// })();
+// This function serves the purpose of helping the user in selecting a path
+// or a free roaming and possibly also the default difficulty for the texts
 
-const welcome = (() => {
-	let welcomeDialog = document.createElement("dialog");
-	let container = document.querySelector("#grid-container");
-})();
+const welcome = document.querySelector("#welcome-dialog");
+const roamfree = document.querySelector("#roamfree");
+const legacy = document.querySelector("#legacypath-btn");
+const origin = document.querySelector("#originpath-btn");
+const map = document.querySelector("svg");
+const container = document.querySelector("#grid-container");
+const nav = document.querySelector("nav");
+
+roamfree.addEventListener("click", () => {
+	welcome.style.display = "none";
+	map.style.display = "block";
+	container.append(nav);
+	nav.className = "nav-interface";
+});
+legacy.addEventListener("click", () => {
+	welcome.style.display = "none";
+	map.style.display = "block";
+	container.append(nav);
+	nav.className = "nav-interface";
+});
+origin.addEventListener("click", () => {
+	welcome.style.display = "none";
+	map.style.display = "block";
+	container.append(nav);
+	nav.className = "nav-interface";
+	const rightmask = document.querySelector("#rightMask");
+	rightmask.style.display = "block";
+});
