d9c2f2884827909bd4d7d96cc7d98b83c6d884e9
Francesco Alaimo <f.alaimo99@gmail.com>
2025-02-04 13:00:45 +0100

    feat/fix(fixed icons issue, improved ui)

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,19 @@
 const adventurers = [
 	{
 		adventurer: "Merry & Pippin",
-		text: "You like to stroll around peacefully, taking your time, but you're not interested in long and complicate explanations. (Perfect for young students and casual visitors who don't have much time)",
+		text: `You like to stroll around peacefully, taking your time, but you're not interested in long and complicate explanations.	
+		<br><br>(Perfect for <b>young students</b> and <b>casual visitors</b> who don't have much time)`,
+		url: `http://screenrant.com/wp-content/uploads/2017/03/Merry-and-Pippin-Are-undervalued-in-LOTR.jpg`,
 	},
 	{
 		adventurer: "Aragorn & Eowin",
 		text: "You want to take action and feel completely satisfied of your visit, unfortunately you're not an elf and you don't have all the time in this world (Ideal for the casual visitors who wants to have a memorable visit, the classic museal experience)",
+		url: `https://c4.wallpaperflare.com/wallpaper/166/202/477/the-lord-of-the-rings-aragorn-horses-viggo-mortensen-eowyn-the-two-towers-miranda-otto-animals-horses-hd-art-wallpaper-preview.jpg`,
 	},
 	{
 		adventurer: "Elrond & Galadriel",
 		text: "You're well versed in the Lore of Arda, these items resound with you in profound ways, but this can also be the beginning that will lead you to learn something unexpected (Texts and insights written having in mind tolkien scholars and university students, who wants to know more about one of the most important narratives of the last century)",
+		url: `https://static0.srcdn.com/wordpress/wp-content/uploads/2021/11/amazons-lord-of-the-rings-right-to-recast-galadriel-and-elrond.jpg`,
 	},
 ];
 
@@ -29,17 +33,19 @@ const welcomeDialog = () => {
 
 const charDialog = () => {
 	const dialog = document.createElement("dialog");
-
+	let counter = 0;
 	const inner = `
             <h1>What kind of Adventurer are you?</h1>
-            <span class="material-symbols-outlined" id='arrow-back'>arrow_back_ios</span>
-            <img src="http://screenrant.com/wp-content/uploads/2017/03/Merry-and-Pippin-Are-undervalued-in-LOTR.jpg"></img>
-            <span class="material-symbols-outlined" id='arrow-forward'>arrow_forward_ios</span>
-            <p>Placeholder</p>`;
+			<h2>${adventurers[counter].adventurer}</h2>
+            <span class="material-icons" id='arrow-back'>arrow_back</span>
+            <img src="${adventurers[counter].url}"></img>
+            <span class="material-icons" id='arrow-forward'>arrow_forward</span>
+            <p>${adventurers[counter].text}</p>
+			<button class='path-button'>Select</button>`;
 	dialog.innerHTML = inner;
 	dialog.className = "charDialog";
 	container.appendChild(dialog);
-	dialog.show();
+	document.onload(dialog.show());
 };
 
 welcomeDialog();
